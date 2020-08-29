3c10d57ead0e95eec1ff0481f0d30afb09089516
TereSolano15 <63326038+TereSolano15@users.noreply.github.com>
2020-08-29 17:19:39 -0600

    Interfaz

---
+++ src/ISendPayment.cpp
@@ -0,0 +1,9 @@
+//
+// Created by Tere Solano on 29/8/2020.
+//
+
+#include "ISendPayment.h"
+
+ISendPayment::~ISendPayment() {
+
+}
---
+++ src/ISendPayment.h
@@ -0,0 +1,17 @@
+//
+// Created by Tere Solano on 29/8/2020.
+//
+
+#ifndef MY_PROJECT_NAME_ISENDPAYMENT_H
+#define MY_PROJECT_NAME_ISENDPAYMENT_H
+#include <sstream>
+using namespace std;
+
+class ISendPayment {
+public:
+    virtual ~ISendPayment();
+    virtual string sendPayment() = 0;
+};
+
+
+#endif //MY_PROJECT_NAME_ISENDPAYMENT_H
